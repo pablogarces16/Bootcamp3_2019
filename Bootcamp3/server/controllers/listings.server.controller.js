bb21732bc256fc85f079115924f7f4645c62c71f
Pablo Garces <pablogarcesee@gmail.com>
2019-10-02 20:29:53 -0400

    completed

--- Bootcamp3/server/controllers/listings.server.controller.js
+++ Bootcamp3/server/controllers/listings.server.controller.js
@@ -1,21 +1,21 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Listing = require('../models/listings.server.model.js'),
     coordinates = require('./coordinates.server.controller.js');
-    
+
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message. 
+  On an error you should send a 404 status code, as well as the error message.
   On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions refer back to this tutorial 
+  HINT: if you are struggling with implementing these functions refer back to this tutorial
   https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
   or
   https://medium.com/@dinyangetoh/how-to-build-simple-restful-api-with-nodejs-expressjs-and-mongodb-99348012925d
-  
 
-  If you are looking for more understanding of exports and export modules - 
+
+  If you are looking for more understanding of exports and export modules -
   https://www.sitepoint.com/understanding-module-exports-exports-node-js/
   or
   https://adrianmejia.com/getting-started-with-node-js-modules-require-exports-imports-npm-and-beyond/
@@ -28,16 +28,16 @@ exports.create = function(req, res) {
   var listing = new Listing(req.body);
 
   /* save the coordinates (located in req.results if there is an address property) */
-  if(req.results) {
+  if (req.results) {
     listing.coordinates = {
-      latitude: req.results.lat, 
+      latitude: req.results.lat,
       longitude: req.results.lng
     };
   }
- 
+
   /* Then save the listing */
   listing.save(function(err) {
-    if(err) {
+    if (err) {
       console.log(err);
       res.status(400).send(err);
     } else {
@@ -57,11 +57,28 @@ exports.read = function(req, res) {
 exports.update = function(req, res) {
   var listing = req.listing;
 
-  /* Replace the listings's properties with the new properties found in req.body */
- 
-  /*save the coordinates (located in req.results if there is an address property) */
- 
-  /* Save the listing */
+  // update info if it exists
+  if (req.body.name) listing.name = req.body.name;
+  if (req.body.code) listing.code = req.body.code;
+  if (req.body.address) listing.address = req.body.address;
+
+  // update the date
+  listing.updated_at = new Date();
+
+  // save coordinates
+  if (req.results) {
+    listing.coordinates.latitude = req.results.lat;
+    listing.coordinates.longitude = req.results.lng;
+  }
+
+  // save the listing
+  listing.save(function(err) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.json(listing);
+    }
+  });
 
 };
 
@@ -70,24 +87,38 @@ exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Add your code to remove the listins */
+  listing.deleteOne(listing, function(err) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.json(listing);
+    }
+  });
 
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Add your code */
+  Listing.find().sort('code').exec(function(err, listings) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.json(listings);
+    }
+  });
 };
 
-/* 
-  Middleware: find a listing by its ID, then pass it to the next request handler. 
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
 
-  HINT: Find the listing using a mongoose query, 
-        bind it to the request object as the property 'listing', 
+  HINT: Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
         then finally call next
  */
 exports.listingByID = function(req, res, next, id) {
   Listing.findById(id).exec(function(err, listing) {
-    if(err) {
+    if (err) {
       res.status(400).send(err);
     } else {
       req.listing = listing;
